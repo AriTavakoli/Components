4250424e7ae850de39139734f273d46a442ec9a9
Ari Tavakoli <aritavakoli98@gmail.com>
2023-01-18 02:43:13 +0000

    Update Filter.jsx

--- src/Features/SearchV2/components/Filter/Filter.jsx
+++ src/Features/SearchV2/components/Filter/Filter.jsx
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
-import { useSearchDispatch, useSearchContext } from '../SearchProvider';
-import './Filter.css'
+import React, { useState } from "react";
+import { useSearchDispatch, useSearchContext } from "../SearchProvider";
+import "./Filter.css";
 const Filter = () => {
-
-  const { searchTerm, selectedCategories, toggleCategory, availableCategories, } = useSearchContext();
+  const {
+    searchTerm,
+    selectedCategories,
+    toggleCategory,
+    availableCategories,
+  } = useSearchContext();
 
   return (
     <div className="filter-container">
-
-
       <div className="filter-available">
-        {availableCategories.map(category => (
-          <div key={category.id} className="filter-button" onClick={() => toggleCategory(category)}>
+        {availableCategories.map((category) => (
+          <div
+            key={category.id}
+            className="filter-button"
+            onClick={() => toggleCategory(category)}
+          >
             <span>{category.name}</span>
             {/* When the user clicks on the "+" icon, add the category to the selected categories list */}
-            <svg onClick={() => toggleCategory(category)} width="12" height="12" style={{ alignItems: 'center' }}>
+            <svg
+              onClick={() => toggleCategory(category)}
+              width="12"
+              height="12"
+              style={{ alignItems: "center" }}
+            >
               <path d="M6 2V10M2 6H10" stroke="currentColor" stroke-width="2" />
             </svg>
           </div>
@@ -26,12 +37,9 @@ const Filter = () => {
 
 export default Filter;
 
-
 // {/* <div className="filter-selected">
 // {selectedCategories.map(category => (
 //   <div key={category.id} className="filter-button" onClick={() => toggleCategory(category)}>
-
-
 
 //     <span>{category.name}</span>
 //     {/* When the user clicks on the "x" icon, remove the category from the selected categories list */}
